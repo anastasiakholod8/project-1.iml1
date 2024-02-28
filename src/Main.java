e6fe24fe3fa3cdf870aa432d673da87688514559
anastasiakholod8 <holodanastasia72@gmail.com>
2024-02-28 11:52:08 +0200

    added files

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        int a=10;
+        int b=3;
+        System.out.println(a/b);
+        System.out.println(a%b);
+
+        int k=28;
+        System.out.println(k/10+k%10);
+
+
+        double q=3.5;
+        System.out.println((int)(q+0.5));
+
     }
 }
